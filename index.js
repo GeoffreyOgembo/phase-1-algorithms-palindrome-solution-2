18e2896f4e81e944a1ef7007ffb58a66891cc032
GeoffreyOgembo <geoffereyogembo06@gmail.com>
2022-06-17 00:15:08 -0400

    commit tree e685bf9ab02e5bafb4cbe1e5347bb7d7e5ba287a

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  for (let startIndex = 0; startIndex < word.length / 2; startIndex++) {
+    // compare the letter we're iterating over to the corresponding letter at the end of the string
+    const endIndex = word.length - 1 - startIndex;
+    if (word[startIndex] !== word[endIndex]) {
+      // if the letters don't match, return false
+      return false;
+    }
+  }
+
+  // if we reach the middle, and all the letters match, return true
+  return true;
 }
 
 /* 
@@ -18,8 +29,14 @@ if (require.main === module) {
 
   console.log("");
 
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
 }
 
-module.exports = isPalindrome;
+  
+module.exports = isPalindrome
